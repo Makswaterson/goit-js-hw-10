8eabbd1c9f7deb48e8f59cb876ec160aff4eb020
Maksym Lukianenko <maks.lukyan@gmail.com>
2023-01-17 19:40:47 +0200

    prepared

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-console.log(Notify);
+import fetchCountries from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  searchInput: document.querySelector('#search-box'),
+  countryList: document.querySelector('country-list'),
+  countryInfo: document.querySelector('country-info'),
+};
+
+//   .then(data => {})
+//   .catch(error => {});
